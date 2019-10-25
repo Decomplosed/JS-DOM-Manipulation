22d708c673312e8a88c90ddd4f18a7778b1d8dfb
Bart Frackiewicz <decomplosed@gmail.com>
2019-10-26 00:42:38 +0200

    added functionality to remove and filter tasks

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const form = document.querySelector('#task-form')
 const taskList = document.querySelector('.collection')
 const clearBtn = document.querySelector('.clear-tasks')
-const filter = document.querySelector('#filet')
+const filter = document.querySelector('#filter')
 const taskInput = document.querySelector('#task')
 
 // Load all event listeners
@@ -12,6 +12,9 @@ loadEventListeners()
 
 function loadEventListeners() {
   form.addEventListener('submit', addTask)
+  taskList.addEventListener('click', removeTask)
+  clearBtn.addEventListener('click', clearTasks)
+  filter.addEventListener('keyup', filterTasks)
 }
 
 function addTask(e) {
@@ -34,4 +37,32 @@ function addTask(e) {
   taskInput.value = '';
 
   e.preventDefault()
+}
+
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove()
+    }
+  }
+}
+
+function clearTasks() {
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase()
+
+  document.querySelectorAll('.collection-item').forEach(function (task) {
+    const item = task.firstChild.textContent
+
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block'
+    } else {
+      task.style.display = 'none'
+    }
+  })
 }
